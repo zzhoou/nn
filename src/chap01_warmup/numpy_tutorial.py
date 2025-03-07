f4ed407bcd4771504a655d38c6a74e9b20fe6b78
ASL505 <166901661+ASL505@users.noreply.github.com>
2025-03-07 19:59:19 +0800

    Add modify numpy (#35)
    * 熟悉numpy的操作
    
    * Add .gitignore file
    
    * 删除了site文件
    
    * Add matplotlib to requirements.txt and use it in numpy_tutorial.py.
    
    * Add matplotlib to requirements.txt and use it in numpy_tutorial.py. Added simple plotting examples for better understanding of numpy array features.
    
    * 添加了matplotlib到requirements.txt文件中，冲突、删除site文件，直接覆盖代码等问题一并解决了

--- src/chap01_warmup/numpy_tutorial.py
+++ src/chap01_warmup/numpy_tutorial.py
@@ -8,251 +8,210 @@
 # ### numpy 的array操作
 
 # #### 1.导入numpy库
-
-# In[ ]:
+#24.导入matplotlib.pyplot库
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')  # 关键代码,临时指定matplotlib后端代码
 import matplotlib.pyplot as plt
 
-
-
 # #### 2.建立一个一维数组 a 初始化为[4,5,6], (1)输出a 的类型（type）(2)输出a的各维度的大小（shape）(3)输出 a的第一个元素（值为4）
-
-# In[ ]:
-a=np.array([4,5,6])
-print("类型：",type(a))
-print("维度：",a.shape)
-print("第一个元素：",a[0])
-
+a = np.array([4, 5, 6])
+print("a 的类型:", type(a))
+print("输出a 的各维度大小:", a.shape)
+print("输出a 的第一个元素:", a[0])
 
 # #### 3.建立一个二维数组 b,初始化为 [ [4, 5, 6],[1, 2, 3]] (1)输出各维度的大小（shape）(2)输出 b(0,0)，b(0,1),b(1,1) 这三个元素（对应值分别为4,5,2）
+b = np.array([[4, 5, 6], [1, 2, 3]])
+print("\n输各维度的大小:", b.shape)
+print("输出b(0,0):", b[0, 0])
+print("输出b(0,1):", b[0, 1])
+print("输出b(1,1):", b[1, 1])
 
-# In[ ]:
-b=np.array([[4,5,6],[1,2,3]])
-print("维度：",b.shape)
-print("b(0,0):",b[0,0])
-print("b(0,1):",b[0,1])
-print("b(1,1):",b[1,1])
-
-# #### 4.  (1)建立一个全0矩阵 a, 大小为 3x3; 类型为整型（提示: dtype = int）(2)建立一个全1矩阵b,大小为4x5;  (3)建立一个单位矩阵c ,大小为4x4; (4)生成一个随机数矩阵d,大小为 3x2.
-
-# In[ ]:
-a=np.zeros((3,3),dtype=int)
-b=np.ones((4,5))
-c=np.eye(4)
-d=np.random.rand(3,2)
-
-
+# #### 4.  (1)建立一个全0矩阵 a, 大小为 3x3; 类型为整型（提示: dtype = int）(2)建立一个全1矩阵b,大小为4x5;  (3)建立一个单位矩阵c ,大小为4x4; (4)生成一个随机数矩阵d,
+# 大小为 3x2.
+a = np.zeros((3, 3), dtype=int)
+print("\n全0矩阵 a:\n", a)
+b = np.ones((4, 5))
+print("全1矩阵 b:\n", b)
+c = np.eye(4)
+print("单位矩阵 c:\n", c)
+d = np.random.random((3, 2))
+print("随机矩阵 d:\n", d)
 
 # #### 5. 建立一个数组 a,(值为[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]] ) ,(1)打印a; (2)输出  下标为(2,3),(0,0) 这两个数组元素的值
-
-# In[ ]:
-a=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
-print("数组 a:\n",a)
-print("下标(2,3)的元素值：",a[2,3])
-print("下标(0,0)的元素值：",a[0,0])
-
-
-
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+print("\n打印 a:\n", a)
+print("a(2,3):", a[2, 3])
+print("a(0,0):", a[0, 0])
 
 # #### 6.把上一题的 a数组的 0到1行 2到3列，放到b里面去，（此处不需要从新建立a,直接调用即可）(1),输出b;(2) 输出b 的（0,0）这个元素的值
-
-# In[ ]:
-b=a[0:2,2:4]
-print("数组b:\n",b)
-print("b的（0，0）位置元素值：",b[0,0])
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+b = a[0:2, 2:4]
+print("\n输出 b:\n", b)
+print("b(0,0):", b[0, 0])
 
 #  #### 7. 把第5题中数组a的最后两行所有元素放到 c中，（提示： a[1:2, :]）(1)输出 c ; (2) 输出 c 中第一行的最后一个元素（提示，使用 -1                 表示最后一个元素）
-
-# In[ ]:
-c=a[-2:, : ]
-print("数组c:\n",c)
-print("c 中第一行的最后一个元素:",c[0,-1])
-
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+c = a[-2:, :]
+print("\n输出 c:\n", c)
+print("c 中第一行的最后一个元素:", c[0, -1])
 
 # #### 8.建立数组a,初始化a为[[1, 2], [3, 4], [5, 6]]，输出 （0,0）（1,1）（2,0）这三个元素（提示： 使用 print(a[[0, 1, 2], [0, 1, 0]]) ）
+a = np.array([[1, 2], [3, 4], [5, 6]])
+print("\n", a[[0, 1, 2], [0, 1, 0]])
 
-# In[ ]:
-a=np.array([[1,2],[3,4],[5,6]])
-print(a[[0,1,2],[0,1,0]])
-
-
-
-
-# #### 9.建立矩阵a ,初始化为[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]，输出(0,0),(1,2),(2,0),(3,1) (提示使用 b = np.array([0, 2, 0, 1])                     print(a[np.arange(4), b]))
-
-# In[ ]:
-a=np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+# #### 9.建立矩阵a ,初始化为[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]，输出(0,0),(1,2),(2,0),(3,1) (提示使用 b = np.array([0,
+# 2, 0, 1])    print(a[np.arange(4), b]))
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 b = np.array([0, 2, 0, 1])
-print(a[np.arange(4), b])
-
+print("\n", a[np.arange(4), b])
 
 # #### 10.对9 中输出的那四个元素，每个都加上10，然后重新输出矩阵a.(提示： a[np.arange(4), b] += 10 ）
-
-# In[ ]:
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+b = np.array([0, 2, 0, 1])
+print("\n")
 a[np.arange(4), b] += 10
 print(a)
 
-
-
-# ### array 的数学运算
-
 # #### 11.  执行 x = np.array([1, 2])，然后输出 x 的数据类型
-
-# In[ ]:
+print("\n")
 x = np.array([1, 2])
-print("x的数据类型：",type(x))
-
-
+print("输出x 的数据类型:", x.dtype)
 
 # #### 12.执行 x = np.array([1.0, 2.0]) ，然后输出 x 的数据类类型
-
-# In[ ]:
+print("\n")
 x = np.array([1.0, 2.0])
-print("x的数据类型：",x.dtype)
-
+print("输出x 的数据类型:", x.dtype)
 
 # #### 13.执行 x = np.array([[1, 2], [3, 4]], dtype=np.float64) ，y = np.array([[5, 6], [7, 8]], dtype=np.float64)，然后输出 x+y ,和 np.add(x,y)
-
-# In[ ]:
-x = np.array([[1, 2], [3, 4]], dtype=np.float64)
-y = np.array([[5, 6], [7, 8]], dtype=np.float64)
-sum_using_operator = x + y
-sum_using_function = np.add(x, y)
-print("使用+运算符的结果:\n", sum_using_operator)
-print("使用np.add函数的结果:\n", sum_using_function)
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(x + y)
+print(np.add(x, y))
 
 # #### 14. 利用 13题目中的x,y 输出 x-y 和 np.subtract(x,y)
-
-# In[ ]:
-difference_using_operator = x - y
-difference_using_function = np.subtract(x, y)
-print("使用-运算符的结果:\n", difference_using_operator)
-print("使用np.subtract函数的结果:\n", difference_using_function)
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(x - y)
+print(np.subtract(x, y))
 
 # #### 15. 利用13题目中的x，y 输出 x*y ,和 np.multiply(x, y) 还有  np.dot(x,y),比较差异。然后自己换一个不是方阵的试试。
-
-# In[ ]:
-elementwise_multiplication = x * y
-multiplication_using_function = np.multiply(x, y)
-dot_product = np.dot(x, y)
-print("使用*运算符的结果（元素级乘法）:\n", elementwise_multiplication)
-print("使用np.multiply函数的结果（元素级乘法）:\n", multiplication_using_function)
-print("使用np.dot函数的结果（矩阵乘法）:\n", dot_product)
-#换个不是方阵的
-x = np.array([[1, 2, 6], [3, 4, 5]], dtype=np.float64)
-y = np.array([[5, 6], [7, 8], [9, 10]], dtype=np.float64)
-print(x*y)
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(x * y)
 print(np.multiply(x, y))
-print(np.dot(x,y))
-#发现只有np.dot(x,y)可以输出
+print(np.dot(x, y))
 
 # #### 16. 利用13题目中的x,y,输出 x / y .(提示 ： 使用函数 np.divide())
-
-# In[ ]:
-division_using_operator = x / y
-division_using_function = np.divide(x, y)
-print("使用/运算符的结果（元素级除法）:\n", division_using_operator)
-print("使用np.divide函数的结果（元素级除法）:\n", division_using_function)
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(x / y)
+print(np.divide(x, y))
 
 # #### 17. 利用13题目中的x,输出 x的 开方。(提示： 使用函数 np.sqrt() )
-
-# In[ ]:
-square_root_of_x=np.sqrt(x)
-print("x的平方根:\n", square_root_of_x)
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(np.sqrt(x))
 
 # #### 18.利用13题目中的x,y ,执行 print(x.dot(y)) 和 print(np.dot(x,y))
-
-# In[ ]:
-print("使用x.dot(y)的结果：\n",x.dot(y))
-print("使用np.dot(x, y)的结果：\n",np.dot(x,y))
-
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(x.dot(y))
+print(np.dot(x, y))
 
 # ##### 19.利用13题目中的 x,进行求和。提示：输出三种求和 (1)print(np.sum(x)):   (2)print(np.sum(x，axis =0 ));   (3)print(np.sum(x,axis = 1))
-
-# In[ ]:
-print("(1) x中所有元素的总和:\n", np.sum(x))
-print("(2) 沿着axis=0（列方向）的总和:\n", np.sum(x,axis=0))
-print("(3) 沿着axis=1（行方向）的总和:\n", np.sum(x,axis=1))
-#axis = 0代表列方向，axis = 1代表行方向
-
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+y = np.array([[5, 6], [7, 8]], dtype=np.float64, )
+print(np.sum(x))
+print(np.sum(x, axis=0))
+print(np.sum(x, axis=1))
 
 # #### 20.利用13题目中的 x,进行求平均数（提示：输出三种平均数(1)print(np.mean(x)) (2)print(np.mean(x,axis = 0))(3) print(np.mean(x,axis =1))）
-
-# In[ ]:
-print("(1) x中所有元素的平均值:\n",np.mean(x))
-print("(2) 沿着axis=0（列方向）的平均值:\n",np.mean(x,axis=0) )
-print("(3) 沿着axis=1（行方向）的平均值:\n", np.mean(x,axis=1))
-
-
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+print(np.mean(x))
+print(np.mean(x, axis=0))
+print(np.mean(x, axis=1))
 
 # #### 21.利用13题目中的x，对x 进行矩阵转置，然后输出转置后的结果，（提示： x.T 表示对 x 的转置）
-
-# In[ ]:
-x_transposed=x.T
-print("x的转置矩阵:\n", x_transposed)
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+transposed_x = x.T
+print(transposed_x)
 
 # #### 22.利用13题目中的x,求e的指数（提示： 函数 np.exp()）
-
-# In[ ]:
-print("x中每个元素的自然指数:\n", np.exp(x))
-
-
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+print(np.exp(x))
 
 # #### 23.利用13题目中的 x,求值最大的下标（提示(1)print(np.argmax(x)) ,(2) print(np.argmax(x, axis =0))(3)print(np.argmax(x),axis =1))
-
-# In[ ]:
-print("(1) x中最大值的整体索引（展平数组后）:\n", np.argmax(x))
-print("(2) 沿着axis=0（列方向）的最大值索引:\n", np.argmax(x,axis=0))
-print("(3) 沿着axis=1（行方向）的最大值索引:\n", np.argmax(x,axis=1))
-
-
+print("\n")
+x = np.array([[1, 2], [3, 4]], dtype=np.float64, )
+print(np.argmax(x))
+print(np.argmax(x, axis=0))
+print(np.argmax(x, axis=1))
 
 
 
 # #### 24,画图，y=x*x 其中 x = np.arange(0, 100, 0.1) （提示这里用到  matplotlib.pyplot 库）
+print("\n")
+plt.rcParams['figure.dpi'] = 300
 
-# In[ ]:
-x=np.arange(0,100,0.1)
-y=x*x
-plt.figure()
-ax=plt.subplot()
-ax.plot(x,y)
-ax.plot(x, y, color='#FF4500', linewidth=2.5, linestyle='--', alpha=0.8)  
-ax.grid(True, which='both', linestyle=':', linewidth=0.5)  
-ax.set_title('Plot of y = x^2')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-plt.tight_layout()  
+# 生成 x 值
+x = np.arange(0, 100, 0.1)
+# 计算对应的 y 值
+y = x * x
+
+# 创建图形并绘制曲线
+plt.figure(figsize=(5, 3))
+plt.plot(x, y, label='$y = x^2$', color='blue')
+
+# 添加标题和标签
+plt.title('Graph of $y = x^2$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# 显示图例
+plt.legend()
+
+# 显示网格线
+plt.grid(True)
+
+# 显示图形
 plt.show()
+
+
 # #### 25.画图。画正弦函数和余弦函数， x = np.arange(0, 3 * np.pi, 0.1)(提示：这里用到 np.sin() np.cos() 函数和 matplotlib.pyplot 库)
+print("\n")
+plt.rcParams['figure.dpi'] = 300
 
-# In[ ]:
-x=np.arange(0,3*np.pi,0.1)
-y_sin=np.sin(x)
-y_cos=np.cos(x)
-plt.figure()
-ax=plt.subplot()
-ax.plot(x,y_sin,label='sin(x)')
-ax.plot(x, y_cos, label='cos(x)')
-ax.set_title('Plot of Sine and Cosine Functions')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.legend()
+#生成x值
+x = np.arange(0, 3 * np.pi, 0.1)
+#计算正弦余弦的值
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+#绘制图形并绘制曲线
+plt.figure(figsize=(5, 3))
+plt.plot(x, y1, label='$y = np.sin(x)$', color='blue')
+plt.plot(x, y2, label='$y = np.cos(x)$', color='green')
+
+#添加标题跟标签
+plt.title('$y1 = np.sin(x)$, $y2 = np.cos(x)$')
+plt.xlabel('x')
+plt.ylabel('y')
+#显示图例
+plt.legend()
+#显示网格线
+plt.grid(True)
+
+#显示图形
 plt.show()
+
