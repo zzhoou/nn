3a887513638fb5752383ee41fa6200ea25d2c7b1
congci <157966630+congci6@users.noreply.github.com>
2025-04-29 07:54:25 +0800

    添加部分注释 (#121)
    * 优化注释
    
    * 添加部分注释
    
    ---------
    
    Co-authored-by: congci <2823714488@qq.com>

--- src/chap02_linear_regression/exercise-linear_regression.py
+++ src/chap02_linear_regression/exercise-linear_regression.py
@@ -57,6 +57,7 @@ def gaussian_basis(x, feature_num=10):
 # In[6]:
 def identity_basis(x):
     # 在 x 的最后一个维度上增加一个维度，将其转换为二维数组
+    # 用于适配线性回归的矩阵运算格式 
     ret = np.expand_dims(x, axis=1)
     return ret
 
@@ -67,6 +68,7 @@ def multinomial_basis(x, feature_num=10):
     #==========
     #todo '''请实现多项式基函数'''
     # 在 x 的最后一个维度上增加一个维度，将其转换为三维数组
+    # 通过列表推导式创建各次项，最后在列方向拼接合并
     x = np.expand_dims(x, axis=1) # shape(N, 1)
     # 生成 1, x, x^2, ..., x^(feature_num-1)
     ret = [x**i for i in range(1, feature_num+1)]
