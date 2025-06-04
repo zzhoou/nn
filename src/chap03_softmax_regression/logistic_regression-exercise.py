cbca9d4b41a59bbd931525be2f7cec3fb41242ea
李杰 <114840886+NIANBAIFOX@users.noreply.github.com>
2025-06-04 22:54:36 +0800

    调整logistic_regression-exercise.py文件的注释格式 (#371)
    * Add files via upload
    
    给softmax_regression-exercise.py增加了注释
    
    * Update tf2.0-exercise.py
    
    修改了sigmoid函数的注释
    
    * Update logistic_regression-exercise.py
    
    调整了logistic_regression-exercise.py文件的注释格式

--- src/chap03_softmax_regression/logistic_regression-exercise.py
+++ src/chap03_softmax_regression/logistic_regression-exercise.py
@@ -23,8 +23,10 @@ import matplotlib.cm as cm
 import numpy as np
 
 # 设置随机种子（确保结果可复现）
-np.random.seed(42)      # NumPy的随机种子
-tf.random.set_seed(42)  # TensorFlow的随机种子
+# NumPy的随机种子
+np.random.seed(42)
+# TensorFlow的随机种子
+tf.random.set_seed(42)
 
 # 确保在Jupyter Notebook中内联显示图形
 get_ipython().run_line_magic('matplotlib', 'inline')
