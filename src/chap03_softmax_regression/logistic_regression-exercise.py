44859bc1f93c318c4bbb499d23e237e9b4b9bc0f
tanzhiwei666 <1323570293@qq.com>
2025-06-04 23:07:28 +0800

    修改格式，添加空格 (#353)

--- src/chap03_softmax_regression/logistic_regression-exercise.py
+++ src/chap03_softmax_regression/logistic_regression-exercise.py
@@ -53,9 +53,9 @@ y = np.zeros(dot_num)
 C2 = np.array([x_n, y_n, y]).T
 
 # 绘制正样本，用蓝色加号表示
-plt.scatter(C1[:, 0], C1[:, 1], c='b', marker='+')
+plt.scatter(C1[:, 0], C1[:, 1], c = 'b', marker = '+')
 # 绘制负样本，用绿色圆圈表示
-plt.scatter(C2[:, 0], C2[:, 1], c='g', marker='o')
+plt.scatter(C2[:, 0], C2[:, 1], c = 'g', marker = 'o')
 
 # 将正样本和负样本连接成一个数据集
 data_set = np.concatenate((C1, C2), axis=0)
