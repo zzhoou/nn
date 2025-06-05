9b2fb13b16cfeaa01647b9cd4ff2678d06b1a28e
jcbiubcuircb <1015489420@qq.com>
2025-06-05 14:13:22 +0800

    提交所有关于RBM的注释修改

--- src/chap12_RBM/rbm.py
+++ src/chap12_RBM/rbm.py
@@ -65,13 +65,11 @@ class RBM:
                 v0 = batch.astype(np.float64)  # 确保数据类型正确
 
                 # 正相传播：从v0计算隐藏层激活概率
-
-
-                h0_prob = self._sigmoid(np.dot(v0, self.W) + self.b_h)
+                h0_prob = self._sigmoid(np.dot(v0, self.W) + self.b_h) # 通过输入层向量v0与权重矩阵self.W的点积，再加上隐藏层偏置self.b_h
                 h0_sample = self._sample_binary(h0_prob)
 
                 # 负相传播：从隐藏层重构可见层，再计算隐藏层概率
-                v1_prob = self._sigmoid(np.dot(h0_sample, self.W.T) + self.b_v)     
+                v1_prob = self._sigmoid(np.dot(h0_sample, self.W.T) + self.b_v)  #将上述结果传入 Sigmoid 激活函数进行非线性变换，得到最终的概率值 v1_prob
                 v1_sample = self._sample_binary(v1_prob)        # 对可见层进行二值采样
                 h1_prob = self._sigmoid(np.dot(v1_sample, self.W) + self.b_h)
 
