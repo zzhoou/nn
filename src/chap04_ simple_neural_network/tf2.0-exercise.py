78421047d6824fa8619d3227ab52f03eba4bd092
ZSJ651 <3244097908@qq.com>
2025-06-06 18:28:10 +0800

    添加注释 (#848)

--- src/chap04_ simple_neural_network/tf2.0-exercise.py
+++ src/chap04_ simple_neural_network/tf2.0-exercise.py
@@ -42,8 +42,8 @@ test_data = np.random.normal(size=[10, 5])
 # ## 实现sigmoid函数
 
 def sigmoid(x):
-    exp_neg_x = tf.exp(-x)
-    prob_x = 1.0 / (1.0 + exp_neg_x)
+    exp_neg_x = tf.exp(-x)# 计算 -x 的指数
+    prob_x = 1.0 / (1.0 + exp_neg_x) # 计算 sigmoid 函数值
     return prob_x
 
 
