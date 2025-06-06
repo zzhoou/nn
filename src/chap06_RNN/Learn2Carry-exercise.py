a13143d110f07b82a240130696395348f9307593
ZSJ651 <3244097908@qq.com>
2025-06-06 18:29:38 +0800

    添加空格使格式美观 (#851)

--- src/chap06_RNN/Learn2Carry-exercise.py
+++ src/chap06_RNN/Learn2Carry-exercise.py
@@ -146,7 +146,7 @@ def train(steps, model, optimizer):
     accuracy = 0.0
     for step in range(steps):
         datas = gen_data_batch(batch_size=200, start=0, end=555555555)
-        Nums1, Nums2, results = prepare_batch(*datas, maxlen=11)
+        Nums1, Nums2, results = prepare_batch(*datas, maxlen = 11)
         loss = train_one_step(model, optimizer, tf.constant(Nums1, dtype=tf.int32),
                               tf.constant(Nums2, dtype=tf.int32),
                               tf.constant(results, dtype=tf.int32))
