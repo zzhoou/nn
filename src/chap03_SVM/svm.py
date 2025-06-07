549c452000c03cc456ff2333b48f2b6c943a2e55
WuXingYue123 <3480427725@qq.com>
2025-06-07 14:05:56 +0800

    Update svm.py (#1238)

--- src/chap03_SVM/svm.py
+++ src/chap03_SVM/svm.py
@@ -26,7 +26,7 @@ class SVM:
     """SVM模型。"""
 
     def __init__(self):
-        self.learning_rate = 0.01
+        self.learning_rate = 0.01 # 学习率：控制梯度下降中参数更新的步长
         self.reg_lambda = 0.01
         self.max_iter = 1000  # 控制模型在优化过程中更新其权重或参数的步长
         self.w = None         # 权重向量
