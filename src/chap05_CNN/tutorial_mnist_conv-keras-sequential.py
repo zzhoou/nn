8e51e424cbdbb0171e06e8f40948f2a85329009b
康达姆123 <2533726755@qq.com>
2025-06-07 11:08:30 +0800

    Update tutorial_mnist_conv-keras-sequential.py (#1138)
    适当换行

--- src/chap05_CNN/tutorial_mnist_conv-keras-sequential.py
+++ src/chap05_CNN/tutorial_mnist_conv-keras-sequential.py
@@ -33,6 +33,7 @@ def mnist_dataset():
     test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
     test_ds = test_ds.map(prepare_mnist_features_and_labels)
     test_ds = test_ds.take(20000).shuffle(20000).batch(20000)
+    
     return ds, test_ds
 
 
