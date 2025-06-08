6db85da80c4e83e00adf428eb4e73eb7f4cd9a5a
xWhite123 <3013466599@qq.com>
2025-06-08 10:28:47 +0800

    Update numpy_tutorial.py (#1341)

--- src/chap01_warmup/numpy_tutorial.py
+++ src/chap01_warmup/numpy_tutorial.py
@@ -4,7 +4,7 @@
 
 # 1. 导入 numpy 库
 import numpy as np  # 将 numpy 库命名为 np
-import matplotlib
+import matplotlib   # 导入Matplotlib库，用于数据可视化
 import matplotlib.pyplot as plt  # 导入 matplotlib 库并将其命名为 plt
 # import 放一起代码美观
 matplotlib.use('TkAgg')  # 关键代码，临时指定 matplotlib 后端代码，指定 TkAgg 可以确保图形能在标准窗口中正常渲染
