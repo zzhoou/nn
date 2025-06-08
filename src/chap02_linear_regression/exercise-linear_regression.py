f159f70e18f9ae518f868fd38754f1f63b609826
bbacxc <140992528+bbacxc@users.noreply.github.com>
2025-06-08 23:52:10 +0800

    Update exercise-linear_regression.py
    调整注释格式

--- src/chap02_linear_regression/exercise-linear_regression.py
+++ src/chap02_linear_regression/exercise-linear_regression.py
@@ -291,11 +291,11 @@ if __name__ == "__main__":
 
     # 显示结果
 
-    plt.plot(x_train, y_train, "ro", markersize=3)  #  红色点为训练集数据
-    plt.plot(x_test, y_test, "k")  # 红色点为训练集数据
-    plt.plot(x_test, y_test_pred, "k")  # 黑线为预测值（可以用其他颜色区分）
-    plt.xlabel("x")  # 设置x轴的标签
-    plt.ylabel("y")  # 设置y轴的标签
-    plt.title("Linear Regression")  # 设置图表标题
-    plt.legend(["train", "test", "pred"])  # 添加图例，表示每条线的含义
-    plt.show()  # 显示图表
+    plt.plot(x_train, y_train, "ro", markersize=3)  # 红色点为训练集数据
+    plt.plot(x_test, y_test, "k")                   # 红色点为训练集数据
+    plt.plot(x_test, y_test_pred, "k")              # 黑线为预测值（可以用其他颜色区分）
+    plt.xlabel("x")                                 # 设置x轴的标签
+    plt.ylabel("y")                                 # 设置y轴的标签
+    plt.title("Linear Regression")                  # 设置图表标题
+    plt.legend(["train", "test", "pred"])           # 添加图例，表示每条线的含义
+    plt.show()                                      # 显示图表
