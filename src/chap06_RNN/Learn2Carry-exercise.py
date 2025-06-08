c4fd7028f5b2acd4266d2ff55e3fa25906eedc7f
tywanyi <3207330776@qq.com>
2025-06-08 21:36:47 +0800

    添加注释 (#1395)

--- src/chap06_RNN/Learn2Carry-exercise.py
+++ src/chap06_RNN/Learn2Carry-exercise.py
@@ -118,7 +118,7 @@ class myRNNModel(keras.Model):
                                                     batch_input_shape=[None, None])
        
         # 基础RNN单元和RNN层
-        self.rnncell = tf.keras.layers.SimpleRNNCell(64)
+        self.rnncell = tf.keras.layers.SimpleRNNCell(64)#RNN单元（64隐藏层）
         self.rnn_layer = tf.keras.layers.RNN(self.rnncell, return_sequences=True)
         self.dense = tf.keras.layers.Dense(10)
         
