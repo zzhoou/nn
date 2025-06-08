7f01de29b19963112856f809c5aa5021daef7923
Reasle <2411202066@qq.com>
2025-06-08 21:48:48 +0800

    Update poem_generation_with_RNN-exercise.py (#1420)

--- src/chap06_RNN/poem_generation_with_RNN-exercise.py
+++ src/chap06_RNN/poem_generation_with_RNN-exercise.py
@@ -186,7 +186,8 @@ def mkMask(input_tensor, maxLen):
     Returns:
         与input_tensor形状相同的布尔掩码
     """
-    shape_of_input = tf.shape(input_tensor) # 获取输入张量的形状
+    # 获取输入张量的形状
+    shape_of_input = tf.shape(input_tensor) 
     shape_of_output = tf.concat(axis=0, values=[shape_of_input, [maxLen]])
     #使用tf.reshape将input_tensor展平为一维张量oneDtensor。shape=(-1,)表示将张量展平为一维，长度由输入张量的总元素数决定
     oneDtensor = tf.reshape(input_tensor, shape=(-1,))
